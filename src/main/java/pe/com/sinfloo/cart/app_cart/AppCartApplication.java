63ff724811bc78083b7e2be3fa42d9b83f7aae87
Luwy Dyro <luwydyro@gmail.com>
2025-08-22 11:44:38 -0500

    Start Project

---
+++ src/main/java/pe/com/sinfloo/cart/app_cart/AppCartApplication.java
@@ -0,0 +1,13 @@
+package pe.com.sinfloo.cart.app_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/sinfloo/cart/app_cart/AppCartApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.sinfloo.cart.app_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
